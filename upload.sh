861d6a64f54eb6d6b66d9a518b1f7ae489a4a66d
root <root@proctorVM.uhaggwpjgzlejb0hmbei5dmbgd.gx.internal.cloudapp.net>
2019-04-15 16:14:23 +0000

    initial commit

--- upload.sh
+++ upload.sh
@@ -1,10 +1,5 @@
-git init
-<<<<<<< HEAD
-git remote add origin http://github.com/brunoterkaly/api-trips.git
-git pull origin master
-=======
 git remote add origin http://github.com/brunoterkaly/api-trip.git
->>>>>>> 32a8a1d1dc8ebb6a88c1cab6f1c55e7cef52d42c
+git pull origin master
 git add .
 git commit -m 'initial commit'
 git push -u origin master
